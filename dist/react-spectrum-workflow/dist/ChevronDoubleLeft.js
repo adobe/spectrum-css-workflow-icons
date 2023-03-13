b9203c986ae4003b66e341fcbe0a1c887feb12b2
Michael Jordan <mijordan@adobe.com>
2023-03-13 14:23:20 -0400

    fix(#27): Chevron icons: multiple elements having same id should be avoided

--- dist/react-spectrum-workflow/dist/ChevronDoubleLeft.js
+++ dist/react-spectrum-workflow/dist/ChevronDoubleLeft.js
@@ -27,12 +27,10 @@ function A4uChevronDoubleLeft(_ref) {
     viewBox: "0 0 36 36"
   }, props, props), /*#__PURE__*/_react["default"].createElement("path", {
     fillRule: "evenodd",
-    d: "M6,18v0a1.988,1.988,0,0,0,.585,1.409l7.983,7.98a2,2,0,1,0,2.871-2.772l-.049-.049L10.819,18l6.572-6.57a2,2,0,0,0-2.773-2.87l-.049.049-7.983,7.98A1.988,1.988,0,0,0,6,18Z",
-    id: "S_MillerColumnsChevronPrevious"
+    d: "M6,18v0a1.988,1.988,0,0,0,.585,1.409l7.983,7.98a2,2,0,1,0,2.871-2.772l-.049-.049L10.819,18l6.572-6.57a2,2,0,0,0-2.773-2.87l-.049.049-7.983,7.98A1.988,1.988,0,0,0,6,18Z"
   }), /*#__PURE__*/_react["default"].createElement("path", {
     fillRule: "evenodd",
     d: "M18,18v0a1.988,1.988,0,0,0,.585,1.409l7.983,7.98a2,2,0,1,0,2.871-2.772l-.049-.049L22.819,18l6.572-6.57a2,2,0,0,0-2.773-2.87l-.049.049-7.983,7.98A1.988,1.988,0,0,0,18,18Z",
-    "data-name": "S_MillerColumnsChevronPrevious",
-    id: "S_MillerColumnsChevronPrevious-2"
+    "data-name": "S_MillerColumnsChevronPrevious"
   }));
 }
