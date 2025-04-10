4589d87d8a7b24df0958a6c6758b90ae034d8c51
Patrick Fulton <pfulton@adobe.com>
2025-04-10 09:15:18 -0400

    feat: update to 4.1.0 a4u release

---
+++ icons/assets/components/iconCommentHide.d.ts
@@ -0,0 +1,10 @@
+import { TemplateResult } from 'lit';
+    export declare const iconCommentHide: ({ width, height, ariaHidden, title, id, focusable }?: {
+        width?: number | undefined;
+        height?: number | undefined;
+        ariaHidden?: boolean | undefined;
+        title?: string | undefined;
+        id?: string | undefined;
+        focusable?: boolean | undefined;
+    }) => string | TemplateResult;
+    export default iconCommentHide;
